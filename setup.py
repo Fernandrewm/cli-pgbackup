b98fb85c23452d061e903e6a0be2099d1915775c
Fernando <fernandrewm@gmail.com>
2022-02-15 01:11:39 -0600

    Wiring the pieces together

---
+++ setup.py
@@ -0,0 +1,24 @@
+from importlib.metadata import entry_points
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Fernando Muñoz',
+    author_email='fernandrewm@gmail.com',
+    description='A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_type='text/markdown',
+    url='https://github.com/Fernandrewm/cli-pgbackup',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requieres=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ],
+    }
+)
--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,4 +1,6 @@
 from argparse import Action, ArgumentParser
+from ast import arg
+from pydoc import cli
 from unicodedata import name
 
 
@@ -17,11 +19,32 @@ def create_parser():
     )
 
     parser.add_argument("url", help="URL of the database to backup")
-    parser.add_argument("--driver",
+    parser.add_argument("--driver", '-d',
                         help="how & where to store backup",
                         nargs=2,
+                        metavar=("DRIVER", "DESTINATION"),
                         action=DriverAction,
                         required=True
                         )
 
     return parser
+
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H-%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(
+            f"Backing database up to {args.destination} in s3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {oufile.name}")
+        storage.local(dump.stdout, outfile)
