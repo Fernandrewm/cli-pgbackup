7486d02dfda6dfac345f1a27ac86271435833a80
Fernando <fernandrewm@gmail.com>
2022-02-15 00:40:54 -0600

    S3 interactions

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -5,3 +5,7 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
